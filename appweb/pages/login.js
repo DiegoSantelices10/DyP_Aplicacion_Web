4b27f29624f65b878884db7bdc2987b81d6bf219
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2022-05-24 13:35:06 -0300

    preparando el Index

--- appweb/pages/login.js
+++ appweb/pages/login.js
@@ -12,7 +12,6 @@ export default function Login() {
           <Image src="/images/liebreblack.png" width={95} height={80} />
           <h1 className=" text-3xl font-bold">¡Registra tu negocio!</h1>
           <p className=" text-md font-medium w-2/3">Registrate en simples pasos y empieza a hacer crecer tu negocio junto a nosotros.</p>
-
         </div>
         <div className="grid content-center rounded-md shadow-lg bg-white w-full 
           h-full p-8 lg:w-5/12 lg:h-auto md:w-1/2 md:h-auto">
@@ -20,14 +19,13 @@ export default function Login() {
             <h1 className="font-bold"> ¡Bienvenido, ingresá a tu cuenta!</h1>
           </div>
           <Form />
-          <div class="flex items-center my-6 before:flex-1 
+          <div className="flex items-center my-6 before:flex-1 
                     before:border-t before:border-gray-300 
                     before:mt-0.5 after:flex-1 after:border-t 
                     after:border-gray-300 after:mt-0.5"
           >
             <p className="text-center text-xs text-slate-400 mx-4 mb-0">O Registrate con</p>
           </div>
-
           <div className="text-center mx-auto ">
             <button onClick={() => signIn("google")} className="flex items-center" type="button">
               <Image src="/images/LogoGoogle.png" width={30} height={30} />
@@ -48,7 +46,7 @@ export const getServerSideProps = async (context) => {
   if (session) {
     return {
       redirect: {
-        destination: "/signUp",
+        destination: "/",
         permanent: false,
       },
     };
